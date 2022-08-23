6300910b2687ea01bf64e55f1353f866f7feedeb
Kaan Gemalmazoğlu <kaangemalmaz@gmail.com>
2022-08-23 18:16:54 +0300

    Onion.Net-2

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+            private readonly BrandBusinessRules _brandBusinessRules;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+                _brandBusinessRules = brandBusinessRules;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                //business rules
+                await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+                //map + insert
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand); //veritabanından gelen nesne ön yüze yollanacak.
+                //return
+                return createdBrandDto;
+
+            }
+        }
+    }
+}
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,5 +1,5 @@
-
-
+using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices();
 //builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddInfrastructureServices();
@@ -25,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
